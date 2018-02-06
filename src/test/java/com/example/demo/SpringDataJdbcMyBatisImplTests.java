09b3add3dbfc96dfe3e242e5fdfabb52228e5b59
Kazuki Shimizu <kazuki43zoo@gmail.com>
2018-02-06 23:26:18 +0900

    Apply latest build-snapshot $ spring-boot 2.0.0.RC1

--- src/test/java/com/example/demo/SpringDataJdbcMyBatisImplTests.java
+++ src/test/java/com/example/demo/SpringDataJdbcMyBatisImplTests.java
@@ -1,11 +1,8 @@
 package com.example.demo;
 
-import java.util.Arrays;
-
-import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jdbc.core.CascadingDataAccessStrategy;
 import org.springframework.data.jdbc.core.DataAccessStrategy;
 import org.springframework.data.jdbc.mybatis.MyBatisDataAccessStrategy;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
@@ -17,9 +14,8 @@ public class SpringDataJdbcMyBatisImplTests extends AbstractSpringDataJdbcTests 
 	@EnableJdbcRepositories(repositoryImplementationPostfix = "MyBatisImpl")
 	public static class SpringDataJdbcConfig {
 		@Bean
-		DataAccessStrategy dataAccessStrategy(SqlSessionFactory sqlSessionFactory) {
-			return new CascadingDataAccessStrategy(Arrays.asList(new MyBatisDataAccessStrategy(sqlSessionFactory)));
-//			return new MyBatisDataAccessStrategy(sqlSessionFactory);
+		DataAccessStrategy dataAccessStrategy(SqlSession sqlSession) {
+			return new MyBatisDataAccessStrategy(sqlSession);
 		}
 	}
 
